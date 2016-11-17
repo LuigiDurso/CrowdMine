b67973d323b1730d0074baac62024d99c8cff9ce
GiovanniLeo <johnny.1995@hotmail.it>
2016-11-17 09:54:52 +0100

    Fix div overlapping

--- core/view/visitaProfiloPersonale.php
+++ core/view/visitaProfiloPersonale.php
@@ -433,7 +433,7 @@
                         <!--Feedback-->
                         <div role="tabpanel" class="tab-pane" id="tab5">
                             <div class="row">
-                                <div class="col-md-3 col-sm-12">
+                                <div class="col-md-3 col-xs-12">
                                     <div class="section">
                                         <div class="section-title">
                                             Your user name
@@ -445,25 +445,35 @@
                                         <div class="section-title">
                                             Rating
                                         </div>
-                                        <div class="section-body">
-                                            <div class="stars">
-                                                <form action="">
-                                                    <input class="star star-5" id="star-5" type="radio" name="star"/>
-                                                    <label class="star star-5" for="star-5"></label>
-                                                    <input class="star star-4" id="star-4" type="radio" name="star"/>
-                                                    <label class="star star-4" for="star-4"></label>
-                                                    <input class="star star-3" id="star-3" type="radio" name="star"/>
-                                                    <label class="star star-3" for="star-3"></label>
-                                                    <input class="star star-2" id="star-2" type="radio" name="star"/>
-                                                    <label class="star star-2" for="star-2"></label>
-                                                    <input class="star star-1" id="star-1" type="radio" name="star"/>
-                                                    <label class="star star-1" for="star-1"></label>
-                                                </form>
+                                        <div class="section-body" >
+                                            <div class="rating">
+                                                <input type="radio" id="star5" name="rating" value="5"/><label
+                                                    class="full" for="star5" title="Awesome - 5 stars"></label>
+                                                <input type="radio" id="star4half" name="rating" value="4.5"/><label
+                                                    class="half" for="star4half"
+                                                    title="Pretty good - 4.5 stars"></label>
+                                                <input type="radio" id="star4" name="rating" value="4"/><label
+                                                    class="full" for="star4" title="Pretty good - 4 stars"></label>
+                                                <input type="radio" id="star3half" name="rating" value="3.5"/><label
+                                                    class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                                                <input type="radio" id="star3" name="rating" value="3"/><label
+                                                    class="full" for="star3" title="Meh - 3 stars"></label>
+                                                <input type="radio" id="star2half" name="rating" value="2.5"/><label
+                                                    class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                                                <input type="radio" id="star2" name="rating" value="2"/><label
+                                                    class="full" for="star2" title="Kinda bad - 2 stars"></label>
+                                                <input type="radio" id="star1half" name="rating" value="1.5"/><label
+                                                    class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                                                <input type="radio" id="star1" name="rating" value="1"/><label
+                                                    class="full" for="star1" title="Sucks big time - 1 star"></label>
+                                                <input type="radio" id="starhalf" name="rating" value="0.5"/><label
+                                                    class="half" for="starhalf"
+                                                    title="Sucks big time - 0.5 stars"></label>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-7 col-sm-12">
+                                <div class="col-md-7 col-xs-12">
                                     <input type="text" class="form-control"
                                            placeholder="Inserisci il titolo del feedback">
                                     <textarea name="name" rows="3" class="form-control"
