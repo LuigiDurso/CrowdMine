a30879a1684f0efa576eba58782e6156b115d939
Angelo Caputo <a.caputo1991@gmail.com>
2016-12-02 18:57:02 +0100

    aggiunti controlli sull'input dell'utente

--- core/control/inserisciAnnuncio.php
+++ core/control/inserisciAnnuncio.php
@@ -7,20 +7,23 @@
  */
 include_once MANAGER_DIR . 'AnnuncioManager.php';
 
-    $idUtente = $_POST['idUtente'];
-    $data = $_POST['data'];
-    $titolo = $_POST['titolo'];
-    $descrizione = $_POST['descrizione'];
-    $luogo = $_POST["luogo"];
-    $stato = $_POST['stato'];
-    $retribuzione = $_POST['retribuzione'];
-    $tipologia = $_POST['tipologia'];
-    $listaMicrocategorie = $_POST['microcategorie'];
+    $utente = unserialize($_SESSION['utente']);
+    $idUtente = $utente->getId();
+    $titolo = strip_tags(htmlspecialchars(addslashes($_POST['titolo'])));
+    $descrizione = strip_tags(htmlspecialchars(addslashes($_POST['descrizione'])));
+    $luogo = strip_tags(htmlspecialchars(addslashes($_POST['ruolo'])));
+    $retribuzione = strip_tags(htmlspecialchars(addslashes($_POST['retribuzione'])));
+    $tipologia = strip_tags(htmlspecialchars(addslashes($_POST['tipologia'])));
+    $listaMicrocategorie = strip_tags(htmlspecialchars(addslashes($_POST['microcategorie'])));
     //$macrocategoria = ?? l'utente quando inserisce un'annuncio, seleziona prima la macro, e poi gli vengono visualizzate
     //le micro associate, cosi lui può scegliersi direttamente le micro, giusto?
 
+    $stato = "revisione";
+    $dataPubblicazione = new DateTime();
+    $data = $dataPubblicazione->format("Y-m-d");
     //$managerAnnuncio = new AnnuncioManager();
     //$managerAnnuncio->createAnnuncio($idUtente, $data, $titolo, $descrizione, $luogo, $stato, $retribuzione, $tipologia, $listaMicrocategorie);
 
-header("Location:" . DOMINIO_SITO . "/");
+
+    header("Location:" . DOMINIO_SITO . "/");
 ?>
