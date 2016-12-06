2c95502caf0b67435149a02ef149ec9dcaccd4ac
Giorgio Vitiello <igiorgio94@gmail.com>
2016-12-06 09:41:33 +0100

    Aggiunto id per pulsante “mostraRisultati” e funzione Ajax.

---
+++ core/control/ClassificaUtentiControl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: giorgio
+ * Date: 02/12/16
+ * Time: 11:21
+ */
+
+if(isset($_POST["method"])) {
+    $method = $_POST["method"];
+    if($method=="one") {
+        //call methods/functions here
+    }
+}
